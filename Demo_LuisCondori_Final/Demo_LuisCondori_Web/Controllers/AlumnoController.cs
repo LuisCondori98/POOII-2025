7e556bb5416a5ef58f0df2830aa749480c5546cf
LuisCondori98 <lcondori11@hotmail.com>
2025-12-17 20:47:31 -0500

    agregando controller

--- Demo_LuisCondori_Final/Demo_LuisCondori_Web/Controllers/AlumnoController.cs
+++ Demo_LuisCondori_Final/Demo_LuisCondori_Web/Controllers/AlumnoController.cs
@@ -1,5 +1,6 @@
 ﻿using Demo_LuisCondori.Data;
 using Demo_LuisCondori.Entidad.Abstraccion;
+using Demo_LuisCondori.Entidad.Entidad;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,58 @@ namespace Demo_LuisCondori_Web.Controllers
     public class AlumnoController : Controller
     {
         IAlumnoServicio _alumnoServicio = new AlumnoServicio();
+        ICalificacionServicio _calificacionServicio = new CalificacionServicio();
+
+        public ActionResult Details(string codigo = "")
+        {
+            Alumno alumnoFind = _alumnoServicio.obtenerAlumno(codigo);
+            alumnoFind.Calificaciones = _calificacionServicio.listarCalificaciones(codigo).ToList();
+
+            return View(alumnoFind);
+        }
 
         public ActionResult Index()
         {
             return View(_alumnoServicio.filtrarAlumnos());
+        }
+
+        public ActionResult Delete(string codigo = "")
+        {
+            string alumnoDelete = _alumnoServicio.eliminar_alumno(codigo);
+
+            ViewBag.mensaje = alumnoDelete;
+
+            return View("Index", _alumnoServicio.filtrarAlumnos());
+        }
+
+        public ActionResult Create()
+        {
+
+            return View(new Alumno());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Alumno alumno)
+        {
+
+            string alumnoCreate = _alumnoServicio.insertar_alumno(alumno);
+
+            ViewBag.mensaje = alumnoCreate;
+
+            return View(alumno);
+        }
+
+        public ActionResult AgregarNota()
+        {
+
+            return View(new Calificacion());
+        }
+
+        [HttpPost]
+        public ActionResult AgregarNota(Calificacion calificacion)
+        {
+
+            return View(calificacion);
         }
     }
 }
